6c01ab97c05e0d52b349142e4f6176aff1632cce
kaivanwong <kaivanwong@outlook.com>
2023-02-02 11:50:55 +0800

    feat: add typescript support

--- packages/nestjs/src/app/app.controller.ts
+++ packages/nestjs/src/app/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
+import type { AppService } from './app.service'
+import type { App } from './interfaces/app.interface'
 
-@Controller()
+@Controller('app')
 export class AppController {
+  constructor(private appSerivce: AppService) { }
+
+  @Get()
+  findAll(): App {
+    return this.appSerivce.findAll()
+  }
 }
--- packages/nestjs/src/app/app.service.ts
+++ packages/nestjs/src/app/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import type { App } from './interfaces/app.interface'
+import { app } from './app.config'
 
 @Injectable()
 export class AppService {
+  findAll(): App {
+    return app
+  }
+
   getAppName(): string {
-    return '@kaivanwong/playgrounds-nestjs'
+    return app.name
+  }
+
+  getAppVersion(): string {
+    return app.version
   }
 }
