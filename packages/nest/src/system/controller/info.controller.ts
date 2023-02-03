4bbcabc819faf022d3da5c7797fd27dae75aba4a
kaivanwong <kaivanwong@outlook.com>
2023-02-03 10:37:59 +0800

    feat: add eslint config

--- packages/nest/src/system/controller/info.controller.ts
+++ packages/nest/src/system/controller/info.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common'
 import type { SystemInfo } from '../interfaces/info'
-import type { SystemInfoService } from './../service/info.service'
+import { SystemInfoService } from './../service/info.service'
 
 @Controller('system/info')
 export class SystemInfoController {
