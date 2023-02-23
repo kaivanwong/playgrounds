578d9a9a506bb8d5a96bf6d8cef0336ab0a1a039
kaivanwong <kaivanwong@outlook.com>
2023-02-23 15:58:24 +0800

    add prisma

--- packages/nest/src/main.ts
+++ packages/nest/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { DemoModule } from './demo/demo.module'
 
 NestFactory.create(DemoModule).then(async (app) => {
-  await app.listen(3000)
+  await app.listen(8000)
 }).catch((error) => {
   console.error(error)
 })
